35ab57eab214bf18df3f06d1de52d9830f079c6d
mjay0729 <kl4314@gmail.com>
2022-03-05 09:39:04 +0900

    fix : reinit project

--- velog/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-//import '../styles/globals.css'
-import '../styles/antd.less'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
--- velog/pages/user.tsx
+++
@@ -1,7 +0,0 @@
-const User = () => {
-    return (
-        <h2>유저 화면 입니다.</h2>
-    )
-}
-
-export default User;
